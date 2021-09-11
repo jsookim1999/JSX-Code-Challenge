414039752e2b5d13360d33bb5b8c310c69f11b6f
Jeongsoo Kim <45583401+jsookim1999@users.noreply.github.com>
2021-09-10 21:54:03 -0700

    Code Challenge commit Creates a heading and unordered list

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
-// added changes
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Rendering Heading 1</h1>
+    <ul>
+      <li>Unordered list item 1</li>
+      <li>Unordered list item 2</li>
+      <li>Unordered list item 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
